3ea1e4b577fd6a1c805fc342f7a926f7607a1498
Егор Гущин <gea11@yandex.ru>
2022-07-03 11:00:41 +0300

    на второе ревью

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -7,6 +7,7 @@ from posts.models import Comment, Follow, Group, Post
 
 User = get_user_model()
 
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
 
 from posts.models import Group, Post, User
 from .permissions import IsAuthorOrReadOnly
